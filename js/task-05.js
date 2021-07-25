ac66774a1be842ce7ece51f22cecfec1d04e5932
SlavikChuprin <chuprin.00@yandex.ru>
2021-07-25 22:10:25 +0300

    task-06

--- js/task-05.js
+++ js/task-05.js
@@ -1,6 +1,5 @@
 const inputEl = document.querySelector('#name-input');
 const nameEl = document.querySelector('#name-output');
-console.log(inputEl.value);
 inputEl.addEventListener('input', () => {
     nameEl.textContent = inputEl.value === '' ? 'незнакомец' : inputEl.value;
 });
