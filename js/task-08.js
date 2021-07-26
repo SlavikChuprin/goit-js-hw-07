eb9c99a88ef4819c5437da9a0b1781723d020568
SlavikChuprin <chuprin.00@yandex.ru>
2021-07-26 14:26:24 +0300

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -4,23 +4,33 @@ const creatBtn = document.querySelector('button[data-action="render"]');
 const clearBtn = document.querySelector('button[data-action="destroy"]')
 
 
-const createBoxes = (amount) =>{
- 
+const createBoxes = () =>{
+    const amount = Number(inputEl.value);
+    let k = 20;
     for(let i=1; i<=amount; i+= 1) {
-
-        galleryEl.insertAdjacentHTML('beforeend',`<div id='${i}'></div>`);
-        const el = document.querySelector('#i');
-        console.log(el);
-        console.log(galleryEl.insertAdjacentHTML('beforeend',`<div id='${i}'></div>`));
-        
-        
-        // el.style.wight = '30px';
-        // el.style.height = '30px';
+    
+      const boxEl = document.createElement('div');
+      boxEl.style.backgroundColor = `rgb(${Math.random()*100}, ${Math.random()*100}, ${Math.random()*100})`;
+      boxEl.classList.add('design');  
+      
+      galleryEl.append(boxEl);
+      boxEl.style.width = `${k+10}px`;
+      boxEl.style.height = `${k+10}px`;
+      k = k + 10;
+         
     };
+    
+};
+
+const destroyBoxes = () => {
+
+    galleryEl.innerHTML = '';
+    inputEl.value ='';
 
 };
 
-createBoxes(2);
+creatBtn.addEventListener('click', createBoxes);
+clearBtn.addEventListener('click', destroyBoxes);
 
 
 
